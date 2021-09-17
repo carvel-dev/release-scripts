8135abc974801fb60d3aa29f0db67326ba6fb635
Dennis Leon <leonde@vmware.com>
2021-09-17 15:12:40 -0700

    rename schema to match targeted template
    Authored-by: Dennis Leon <leonde@vmware.com>

--- hack/generate-gh-action-workflows.sh
+++ hack/generate-gh-action-workflows.sh
@@ -4,4 +4,4 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 product=$1
 
-ytt -f ${SCRIPT_DIR}/schema.yml -f ${SCRIPT_DIR}/dist.yml --data-value product=${product} > ${SCRIPT_DIR}/../.github/workflows/dist_${product}.yml
+ytt -f ${SCRIPT_DIR}/dist_schema.yml -f ${SCRIPT_DIR}/dist.yml --data-value product=${product} > ${SCRIPT_DIR}/../.github/workflows/dist_${product}.yml
