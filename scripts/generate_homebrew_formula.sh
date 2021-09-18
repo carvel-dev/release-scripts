a6727a66ea03b7ac8ff62151d0f8ca6fe36eae5e
Dennis Leon <leonde@vmware.com>
2021-09-17 17:06:02 -0700

    Make job name in github action workflow derived by product name in datavalue
    Authored-by: Dennis Leon <leonde@vmware.com>

--- scripts/generate_homebrew_formula.sh
+++ scripts/generate_homebrew_formula.sh
@@ -7,4 +7,9 @@ product_dir=./releases/${product}/
 
 latest_release_file=$(ls ${product_dir} | sort --version-sort --reverse | head -n1)
 
-ytt -f ./scripts/homebrew_formula_schema.yml -f ./scripts/homebrew_formula.yml -f ${product_dir}/${latest_release_file}  -v product=${product} -o json | jq -r .homebrew_formula_template
+ytt \
+  -f ./scripts/homebrew_formula_schema.yml \
+  -f ./scripts/homebrew_formula.yml \
+  -f ${product_dir}/${latest_release_file}  \
+  -v product=${product} \
+  -o json | jq -r .homebrew_formula_template
