1e79299c9e55603e7c1c8dc02df60a12ff42b27a
Dennis Leon <leonde@vmware.com>
2021-10-15 17:07:58 -0700

    Update install.sh from latest version pulled from carvel.dev
    - only updates a specific product version and doesn't 'downgrade' other
    product versions contained in install.sh
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- scripts/generate_homebrew_formula.sh
+++ scripts/generate_homebrew_formula.sh
@@ -8,7 +8,7 @@ product_dir=./releases/${product}/
 latest_release_file=$(ls ${product_dir} | sort --version-sort --reverse | head -n1)
 
 ytt \
-  -f ./scripts/schema.yml \
+  -f ./scripts/homebrew_formula_schema.yml \
   -f ./scripts/homebrew_formula.yml \
   -f ${product_dir}/${latest_release_file}  \
   -v product=${product} \
--- scripts/generate_install_sh.sh
+++ scripts/generate_install_sh.sh
@@ -5,11 +5,16 @@ set -xeu
 product=${1}
 product_dir=./releases/${product}/
 
+path_to_install_sh=${2}
+
 latest_release_file=$(ls ${product_dir} | sort --version-sort --reverse | head -n1)
+previous_release_file=$(ls ${product_dir} | sort --version-sort --reverse | head -n2 | tail -n1)
 
 ytt \
-  -f ./scripts/schema.yml \
+  -f $path_to_install_sh \
+  -f ./scripts/install_sh_schema.yml \
   -f ./scripts/install_sh.yml \
   -f ${product_dir}/${latest_release_file}  \
+  --data-values-file <(ytt -f ${product_dir}/$previous_release_file -f ./scripts/previous_release_dv.yml) \
   -v product=${product} \
   -o json | jq -r .output
